3ccedaafdfbe7eaee2a777b7cce210225f8152c9
DEVTimotonic <timtonic@outlook.fr>
2022-02-01 16:14:51 +0100

    Version 2 de la calculette 'Simple...'

--- lib/katas/simple_calculator.ex
+++ lib/katas/simple_calculator.ex
@@ -1,5 +1,22 @@
-defmodule Calculator do
-  def add("13") do
-    13
+defmodule SimpleCalculator do
+  alias SimpleCalculator.EndLineError
+
+  def add(nb_string) do
+    nb_string
+    |> handle_string()
+    |> String.split([",", "\n"])
+    |> Enum.reduce(0, fn x, acc -> String.to_integer(x) + acc end)
+  end
+
+  defp handle_string(string) do
+    if String.ends_with?(string, [",", "\n"]) do
+      raise EndLineError
+    else
+      string
+    end
+  end
+
+  defmodule EndLineError do
+    defexception message: "It's not possible to have a separator at end of string"
   end
 end
--- test/simple_calculator_test.exs
+++ test/simple_calculator_test.exs
@@ -1,18 +1,55 @@
-defmodule Calculator do
+defmodule SimpleCalculatorTest do
   use ExUnit.Case
-  @moduletag :calculator
+  @moduletag :simple_calculator
+  alias SimpleCalculator.EndLineError
 
-  test "Calculator.add/1 should takes a String and return an Integer" do
-    assert Calculator.add("13") == 13
+  describe "SimpleCaculator" do
+    # 0.
+    test "add/1 should takes a String and returns a integer" do
+      assert SimpleCalculator.add("0") == 0
 
-    assert Calculator.add("127") == 127
+      assert SimpleCalculator.add("42") == 42
+    end
 
-    assert Calculator.add("1300") == 1300
+    # 1.
+    test "add/1 should take to two numbers, separated by commas, and will return their sum as a result" do
+      assert SimpleCalculator.add("1,1") == 2
 
-    assert Calculator.add("9087") == 9087
+      assert SimpleCalculator.add("3,7") == 10
 
-    assert Calculator.add("10072") == 10072
+      assert SimpleCalculator.add("42,58") == 100
+    end
 
-    assert Calculator.add("11191") == 11191
+    # 2.
+    test "add/1 should allow the add function to handle an unknown number of arguments" do
+      assert SimpleCalculator.add("1,1,1") == 3
+
+      assert SimpleCalculator.add("3,7,3,4") == 17
+
+      assert SimpleCalculator.add("42,58,100,2,76") == 278
+    end
+
+    # 3.
+    test "add/1 should Allow the add function to handle newlines as separators, instead of comas" do
+      assert SimpleCalculator.add("1,1\n1") == 3
+
+      assert SimpleCalculator.add("3,7\n3,4") == 17
+
+      assert SimpleCalculator.add("42\n58,100,2,76") == 278
+    end
+
+    # 4.
+    test "add/1 should add validation to not to allow a separator at the end" do
+      assert_raise EndLineError, "It's not possible to have a separator at end of string", fn ->
+        SimpleCalculator.add("3,7\n3,")
+      end
+    end
+
+    # 5.
+    test "add/1 should Allow the add method to handle different delimiters" do
+      assert SimpleCalculator.add("//|1|1|1") == 3
+
+      assert SimpleCalculator.add("//;3;7\n3,4") == 17
+    end
   end
 end
